f5a50bb25907099eca0a73c8170c487f50da4b64
JamesDinnes <cgjamesdinnes@gmail.com>
2019-05-21 21:09:07 +1000

    database updates

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -1,13 +1,20 @@
-const Koa = require("koa");
-const BodyParser = require("koa-bodyparser");
-const Router = require("koa-router");
-const Logger = require("koa-logger");
-const cors = require('koa-cors');
-const serve = require("koa-static");
-const mount = require("koa-mount");
-const HttpStatus = require("http-status");
+require('dotenv').config()
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as BodyParser from 'koa-bodyparser'
+import * as Logger from 'koa-logger'
+import * as cors from 'koa-cors'
+import * as db from './services/users.service'
+
+
+// import * as serve from 'koa-static'
+// const mount = require("koa-mount");
+// const HttpStatus = require("http-status");
+
+
 
 const app = new Koa();
+var router = new Router();
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,5 +23,15 @@ app.use(Logger());
 app.use(cors());
 
 app.listen(3000);
+
+router.get('/users', db.getUsers)
+router.get('/users/:id', db.getUserById)
+router.post('/users', db.createUser)
+router.put('/users/:id', db.updateUser)
+router.delete('/users/:id', db.deleteUser)
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 console.log('Server running on port 3000');
---
+++ src/server/src/services/users.service.ts
@@ -0,0 +1,62 @@
+import { pool } from '../config'
+
+export const getUsers = (request, response) => {
+  console.log(pool)
+  console.log(process.env.DB_USER);
+  
+  // pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  //   if (error) {
+  //     throw error
+  //   }
+  //   response.status(200).json(results.rows)
+  // })
+}
+
+export const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+export const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, result) => {
+    if (error) {
+      throw error 
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
+export const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+export const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, result) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
